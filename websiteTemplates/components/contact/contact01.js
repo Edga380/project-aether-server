2a845831b3620058db58f681427da5500fe5e5b5
Edga380 <a.edgaras44@gmail.com>
2025-06-17 11:15:18 +0100

    Initial 'contact01.js' component

---
+++ websiteTemplates/components/contact/contact01.js
@@ -0,0 +1,18 @@
+module.exports = (data) => {
+  const html = `
+    <!-- Contact start -->
+    <!-- Contact end -->
+    `;
+
+  const css = `
+    /* Contact start */
+    /* Contact end */
+    `;
+
+  const javaScript = `
+    // Contact start
+    // Contact end
+    `;
+
+  return { html: html.trim(), css: css.trim(), javascript: javaScript.trim() };
+};
