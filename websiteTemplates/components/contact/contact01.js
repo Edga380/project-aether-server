26d6cf1715b1355dad7e639f6794a5f75b062eb1
Edga380 <a.edgaras44@gmail.com>
2025-07-10 14:20:40 +0100

    Created 'contact01' component

--- websiteTemplates/components/contact/contact01.js
+++ websiteTemplates/components/contact/contact01.js
@@ -1,11 +1,44 @@
 module.exports = (data) => {
   const html = `
     <!-- Contact start -->
+    <section class="contact">
+      <h2>Contact</h2>
+      <div>
+        <p>${data[0].nameSurname}</p>
+        <p>${data[0].email}</p>
+        <p>${data[0].number}</p>
+      </div>
+    </section>
     <!-- Contact end -->
     `;
 
   const css = `
     /* Contact start */
+    .contact {
+      padding: 4rem 2rem;
+      text-align: center;
+      background-color: var(--bgAltColor);
+      display: flex;
+      flex-direction: column;
+    }
+
+    .contact div {
+      background-color: var(--cardBg);
+      margin: 0 auto;
+      padding: 1rem 4rem 0.5rem 4rem;
+      box-shadow: 0 0 0.25rem rgba(20, 20, 20, 1);
+      border-radius: 0.5rem;
+    }
+
+    .contact h2 {
+      color: var(--primaryColor);
+      margin-bottom: 1rem;
+    }
+
+    .contact p {
+      color: var(--textColor);
+      margin-bottom: 1rem;
+    }
     /* Contact end */
     `;
 
